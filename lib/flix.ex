949ff36b1b07d85457f3777bbe423ac1b48e66f3
Enrique Fernández <enrique@bluelabs.eu>
2021-08-04 13:55:49 +0200

    Update commands' documentation

--- lib/flix.ex
+++ lib/flix.ex
@@ -94,10 +94,23 @@ defmodule Flix do
   ## API
   ## =========================================================================
 
+  @doc """
+  Starts a new `Flix` client without links.
+
+  See `start_link/4` for more details.
+  """
   def start(module, state, host \\ 'localhost', port \\ 5551) do
     GenServer.start(module, [host, port, module, state])
   end
 
+  @doc """
+  Starts a new `Flix` client linked to the current process.
+
+  This is often used to start the `Flix` as part of a supervision tree.
+
+  A `Flix` client is nothing but a `GenServer` sprinkled with some custom logic.
+  See `GenServer.start_link/3` for more details.
+  """
   def start_link(module, state, host \\ 'localhost', port \\ 5551) do
     GenServer.start_link(module, [host, port, module, state])
   end
@@ -114,23 +127,61 @@ defmodule Flix do
   end
 
   @doc """
-  Retrieve current state about the server.
+  Retrieves the current state of the server (ie. `flicd` process the client is connected to).
+
+  After executing this function, `flicd` sends a `Flix.Protocol.Events.GetInfoResponse` event to the client.
   """
   def get_info(client) do
     command = %Flix.Protocol.Commands.GetInfo{}
     send_command(client, command)
   end
 
+  @doc """
+  Creates a scanner with the given `scan_id`.
+
+  After executing this function, `flicd` sends a `Flix.Protocol.Events.AdvertisementPacket` event for each
+  advertisement packet received from a Flic button by the server (ie. `flicd`).
+  `Flix.Protocol.Event.AdvertisementPacket` events are annotated with the provided `scan_id`.
+
+  To stop receiving advertisement events use `remove_scanner/2`.
+
+  If there already exists an active scanner with the provided `scan_id`, the execution of
+  this function has no effect.
+  """
   def create_scanner(client, scan_id) do
     command = %Flix.Protocol.Commands.CreateScanner{scan_id: scan_id}
     send_command(client, command)
   end
 
+  @doc """
+  Removes the scanner with the given `scan_id`.
+
+  The client will stop receiving `Flix.Protocol.Event.AdvertisementPacket` events.
+  """
   def remove_scanner(client, scan_id) do
     command = %Flix.Protocol.Commands.RemoveScanner{scan_id: scan_id}
     send_command(client, command)
   end
 
+  @doc """
+  Creates a connection channel for a Flic button with the given bluetooth address. You assign a unique
+  `conn_id` for this connection channel that will later be used in commands and events to refer to this
+  connection channel.
+
+  After executing this function, `flicd` sends a `Flix.Protocol.Events.CreateConnectionChannelResponse`
+  event to the client.
+
+  When a connection channel is created, the client starts listening for button events originating from the
+  provided Flic button. There are four types of button events (ie. `Flix.Protocol.Events.ButtonUpOrDown`,
+  `Flix.Protocol.Events.ButtonClickOrHold`, `Flix.Protocol.Events.ButtonSingleOrDoubleClick`,
+  `Flix.Protocol.Events.ButtonSingleOrDoubleClickOrHold`). These events are annotated with the provided
+  `conn_id`.
+
+  To stop receiving button events use `remove_connection_channel/2`.
+
+  If there already exists a connection channel with the provided `conn_id`, the execution of this function
+  has no effect.
+  """
   def create_connection_channel(
         client,
         bt_addr,
@@ -150,16 +201,33 @@ defmodule Flix do
     send_command(client, command)
   end
 
+  @doc """
+  Removes a previously created connection channel.
+
+  After executing this function, no further button events will be sent for this channel.
+  If there are no other connection channels active to this Flic button (from neither the provided client
+  nor other clieents), the physical bluetooth connection between `flicd` and the Flic button is
+  disconnected.
+  """
   def remove_connection_channel(client, conn_id) do
     command = %Flix.Protocol.Commands.RemoveConnectionChannel{conn_id: conn_id}
     send_command(client, command)
   end
 
+  @doc """
+  Removes all connection channels among all clients for the specified Flic button.
+  """
   def force_disconnect(client, bt_addr) do
     command = %Flix.Protocol.Commands.ForceDisconnect{bt_addr: bt_addr}
     send_command(client, command)
   end
 
+  @doc """
+  Changes the accepted latency for this connection channel and the auto disconnect time.
+
+  The latency mode is applied immediately but the auto disconnect time is applied the next time
+  the Flic button gets connected.
+  """
   def change_mode_parameters(
         client,
         conn_id,
@@ -177,31 +245,72 @@ defmodule Flix do
     send_command(client, command)
   end
 
+  @doc """
+  Pings the server (ie. `flicd`).
+
+  After executing this function, `flicd` sends a `Flix.Protocol.Event.PingResponse` event to the client.
+  """
   def ping(client, ping_id) do
     command = %Flix.Protocol.Commands.Ping{ping_id: ping_id}
     send_command(client, command)
   end
 
+  @doc """
+  Gets information about a Flic button. The button must have been previously paired with the server (ie.`flicd`).
+
+  After executing thisfunction, `flicd` sends a `Flic.Protocol.Event.GetButtonInfoResponse` to the client.
+  """
   def get_button_info(client, bt_addr) do
     command = %Flix.Protocol.Commands.GetButtonInfo{bt_addr: bt_addr}
     send_command(client, command)
   end
 
+  @doc """
+  Starts a scan wizard with the provided `scan_wizard_id`.
+
+  If there already exists a scan wizard with the same `scan_wizard_id`, the execution of this funcion
+  has no effect.
+  """
   def create_scan_wizard(client, scan_wizard_id) do
     command = %Flix.Protocol.Commands.CreateScanWizard{scan_wizard_id: scan_wizard_id}
     send_command(client, command)
   end
 
+  @doc """
+  Cancels a scan wizard that was previously started.
+
+  If there exists a scan wizard with the provided `scan_wizard_id`, it is cancelled and `flicd` sends a
+  `Flix.Protocol.Events.ScanWizardCompleted` event to the client with the reason set to
+  `Flix.Protocol.Enums.ScanWizardResult.CancelledByUser`.
+  """
   def cancel_scan_wizard(client, scan_wizard_id) do
     command = %Flix.Protocol.Commands.CancelScanWizard{scan_wizard_id: scan_wizard_id}
     send_command(client, command)
   end
 
+  @doc """
+  Deletes a Flic button.
+
+  If the button exists in the list of verified buttons, all connection channels will be removed for
+  all clients for this button. Then, `flicd` sends the `Flix.Protocol.Events.ButtonDeleted` event to all clients.
+
+  If the Flic button does not exist in the list of verified buttons, the execution of this function has no effect
+  but a `Flix.Protocol.Events.ButtonDeleted` is anyways sent to the client.
+  """
   def delete_button(client, bt_addr) do
     command = %Flix.Protocol.Commands.DeleteButton{bt_addr: bt_addr}
     send_command(client, command)
   end
 
+  @doc """
+  Creates a battery status listener for a Flic button.
+
+  If the providede `listener_id` already exists for this client, the execution of this function has no effect.
+
+  After the execution of this function, `flicd` sends a `Flix.Protocol.Events.BatteryStatus` event to the client
+  for the specified button. Further `Flix.Protocol.Events.BatteryStatus` events are sent to the client every time
+  the battery status of the button changes. This does not usually happen more often than every three hours.
+  """
   def create_battery_status_listener(client, listener_id, bt_addr) do
     command = %Flix.Protocol.Commands.CreateBatteryStatusListener{
       listener_id: listener_id,
@@ -211,6 +320,9 @@ defmodule Flix do
     send_command(client, command)
   end
 
+  @doc """
+  Removes the battery status listener identified by the provided `listener_id`.
+  """
   def remove_battery_status_listener(client, listener_id) do
     command = %Flix.Protocol.Commands.RemoveBatteryStatusListener{listener_id: listener_id}
     send_command(client, command)
